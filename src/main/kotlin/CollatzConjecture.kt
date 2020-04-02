4a1477b2a1f67c80e82b4b9235e68ba17cec608d
crystalDf <chendong333@gmail.com>
2020-04-02 21:03:31 +0800

    44 collatz-conjecture

---
+++ src/main/kotlin/CollatzConjecture.kt
@@ -0,0 +1,16 @@
+object CollatzCalculator {
+
+    fun computeStepCount(start: Int): Int {
+
+        require(start > 0)
+
+        return when (start) {
+            1 -> 0
+            else ->
+                when {
+                    start % 2 == 0 -> 1 + computeStepCount(start / 2)
+                    else -> 1 + computeStepCount(3 * start + 1)
+                }
+        }
+    }
+}
